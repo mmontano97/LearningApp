f612d71143366bc6e3b2389c579fcb2802cf6c2e
Michael Montano <michaelmontano40@gmail.com>
2021-03-29 11:42:39 -0600

    -add assets -add data files -add models -parsed json in content model -parsed style data in content model

--- LearningApp/View Models/ContentModel.swift
+++ LearningApp/View Models/ContentModel.swift
@@ -9,6 +9,35 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    var styleData: Data?
     
+    init() {
+        
+        getLocalData()
+    }
     
+    func getLocalData() {
+        
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        do{
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            let jsonDecoder = JSONDecoder()
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            self.modules = modules
+        }
+        catch {
+            print("Couldnt parse local data")
+        }
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+        }
+        catch {
+            print("couldnt parse style data")
+        }
+    }
 }
