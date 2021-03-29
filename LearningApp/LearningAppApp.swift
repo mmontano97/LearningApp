77506eed2bffe7b35a8ecd7330be1e8cd19e7c16
Michael Montano <michaelmontano40@gmail.com>
2021-03-29 10:42:39 -0600

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by michael montano on 2021-03-29.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
