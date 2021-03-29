5ee21a9ac3b3df90a1a6c5ed590432277e4347c9
Michael Montano <michaelmontano40@gmail.com>
2021-03-29 10:48:53 -0600

    -organize and set up file / folders

---
+++ LearningApp/View Models/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by michael montano on 2021-03-29.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
